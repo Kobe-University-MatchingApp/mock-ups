b5b682d616de522b2a1e3b39cd68fce6c24effc0
NAKANO.M <114970041+stumn@users.noreply.github.com>
2025-11-08 22:54:37 +0900

    Example1.jsxのレイアウトを改善し、ヘッダーとナビゲーションを追加

--- vite-mock-ups/src/ui/Navigation.jsx
+++ vite-mock-ups/src/ui/Navigation.jsx
@@ -2,7 +2,7 @@ import Button from './Button'
 
 function Navigation({ prevLink, nextLink }) {
     return (
-        <div className="fixed bottom-8 left-1/2 -translate-x-1/2 flex gap-4">
+        <div className="fixed left-1/2 -translate-x-1/2 flex gap-4">
             <Button to={prevLink}>
                 ← Prev
             </Button>
